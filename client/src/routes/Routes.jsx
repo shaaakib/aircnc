5660880ad19b587283d4a01cd024e9b46642289f
shaaakib <shakibhossainmuhammad@gmail.com>
2024-03-29 05:30:10 +0600

    categories section added

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
+import Home from '../pages/Home'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    children:[{
+      path: '/',
+      element: <Home/>
+    }]
   },
 ])
