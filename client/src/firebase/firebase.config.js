9ef9563ed07315908eba674c8056c14f57756682
shaaakib <shakibhossainmuhammad@gmail.com>
2024-03-29 09:21:55 +0600

    signup page

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlQqK9QLIUlTQJcdzrX0Gn1HHWhY-9Bl0",
-  authDomain: "aircne-ee696.firebaseapp.com",
-  projectId: "aircne-ee696",
-  storageBucket: "aircne-ee696.appspot.com",
-  messagingSenderId: "69887778501",
-  appId: "1:69887778501:web:4bb44c752907e6a26f5aac"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain:  import.meta.env.VITE_authDomain,
+  projectId:  import.meta.env.VITE_projectId,
+  storageBucket:  import.meta.env.VITE_storageBucket,
+  messagingSenderId:  import.meta.env.VITE_messagingSenderId,
+  appId:  import.meta.env.VITE_appId
 };
 
 export const app = initializeApp(firebaseConfig)
